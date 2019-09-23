c42b267ee7e3344194b20d3a902b943a20da1ff0
amserhii11 <amelin.serhii@gmail.com>
2019-09-23 22:07:56 +0300

    Updates

---
+++ precache-manifest.1bd66006893e1c700c13bebb2ed226c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3864bdd597d56cde3fbfa88cc5dc05de",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "4e93a838645240c3b940",
+    "url": "/react_dynamic-list-of-posts/static/css/main.720a106e.chunk.css"
+  },
+  {
+    "revision": "25f285533716dce82f09",
+    "url": "/react_dynamic-list-of-posts/static/js/2.f4de7a07.chunk.js"
+  },
+  {
+    "revision": "4e93a838645240c3b940",
+    "url": "/react_dynamic-list-of-posts/static/js/main.1f372a23.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
